59cd3dbb955261343913cb4e7fe676a594a58629
Ochuodho <ochuodhoderrick@gmail.com>
2023-11-01 18:14:29 +0300

    done

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
 
-db.init_app(app)
+db.init_app(app) #connects our database to our application before it runs
 
 @app.route('/')
 def index():
@@ -25,11 +25,30 @@ def index():
 def pet_by_id(id):
     pet = Pet.query.filter(Pet.id == id).first()
 
-    if not Pet:
+    if not pet:
         response_body = '<h1>404 pet not found</h1>'
         response = make_response(response_body, 404)
         return response
-    
+
+    response_body = f'''
+        <h1>Information for {pet.name}</h1>
+        <h2>Pet Species is {pet.species}</h2>
+        <h2>Pet Owner is {pet.owner.name}</h2>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+
+@app.route('/owner/<int:id>')
+def owner_by_id(id):
+    owner = Owner.query.filter(Owner.id == id).first()
+
+    if not owner:
+        response_body = '<h1>404 owner not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
     response_body = f'<h1>Information for {owner.name}</h1>'
 
     pets = [pet for pet in owner.pets]
@@ -41,11 +60,12 @@ def pet_by_id(id):
         for pet in pets:
             response_body += f'<h2>Has pet {pet.species} named {pet.name}.</h2>'
 
-
-
     response = make_response(response_body, 200)
 
     return response
+
+
+
 
 
 if __name__ == '__main__':
